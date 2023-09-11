c645c6d57798fa750cd211ec0056db3aae55fa20
Jay Kruer <j@dank.systems>
2023-09-10 19:35:59 -0700

    latest changes

--- vcd.el
+++ vcd.el
@@ -102,10 +102,10 @@
                             (split-string (parsec-many-till-as-string
                                            (parsec-any-ch)
                                            (parsec-str "$end")))))
-         (ty (first inner))
-         (size (second inner))
-         (code (third inner))
-         (signal-name (fourth inner)))
+         (ty (car inner))
+         (size (cadr inner))
+         (code (caddr inner))
+         (signal-name (cadddr inner)))
     (list 'declare-signal signal-name code ty size)))
 
 
@@ -117,9 +117,9 @@
 
 (defun parse-var-bureaucracy ()
   (parsec-or
-   (parse-var-decl)
-   (parse-scope)
-   (parse-upscope)))
+   (parsec-try (parse-var-decl))
+   (parsec-try (parse-scope))
+   (parsec-try (parse-upscope))))
 
 (defun parse-vcd-name-decls ()
   (parsec-many1 (parsec-return
@@ -142,7 +142,7 @@ $upscope $end" (parse-vcd-name-decls))
   (parse-vcd-name-decls))
 
 (defun mk-signal-name-table (decls)
-  (let ((name-map (make-hash-table))
+  (let ((name-map (make-hash-table :test 'equal))
         (stk '()))
     (progn (dolist (decl decls)
               (pcase decl
@@ -150,7 +150,7 @@ $upscope $end" (parse-vcd-name-decls))
                 (`(pop) (setf stk (cdr stk)))
                 (`(declare-signal ,signal-name ,code ,ty ,size)
                  (let ((full-name (string-join (reverse (cons signal-name stk)) "."))
-                       (time-map (make-hash-table)))
+                       (time-map (make-hash-table :test 'equal)))
                    (puthash code (list full-name ty size time-map) name-map)))))
            name-map)))
 
@@ -175,60 +175,142 @@ $upscope $end" (parse-vcd-name-decls)))
                             (parsec-eol-or-eof)))))
 
 (defun parse-identifier ()
-  (string-trim (parsec-many1-as-string (parsec-nonwhitespace)))
-  )
+  (string-trim (parsec-many1-as-string (parsec-nonwhitespace))))
 
 (defun parsec-nonwhitespace ()
   (parsec-none-of ?\n ?\t ?\ ))
 
-(defun parse-value ()
-  (string-trim (parsec-many1-as-string (parsec-nonwhitespace)))       ; FIXME
-  )
+(defun parse-scalar-value ()
+  (parsec-one-of ?0 ?1 ?x ?z))
 
-(defun parse-value-change ()
-  (let ((value (parse-value))
+(defun parse-vector-value ()
+  (let ((base (parsec-one-of ?b ?r))
+        (digits (parsec-many1-as-string (parse-scalar-value))))
+    (concat base digits)))
+
+(defun parse-vector-value-change ()
+  (let ((value (parse-vector-value))
         (_ (parsec-many1 (parsec-whitespace)))
         (id (parse-identifier)))
     (list 'chg id value)))
 
-(parsec-with-input "0b1000 )k" (parse-value-change))
+(defun parse-scalar-value-change ()
+  (let ((value (parse-scalar-value))
+        (id (parse-identifier)))
+    (list 'chg id (concat "0b" value))))
+
+(defun parse-value-change ()
+  (parsec-or (parsec-try (parse-vector-value-change))
+             (parsec-try (parse-scalar-value-change))))
+
+(parsec-with-input "b1000 )k" (parse-value-change))
+(parsec-with-input "0#\n0b10000010" (parse-value-change))
 
 (defun parse-value-changes ()
-    (parsec-many1 (parsec-return
-                      (parse-value-change)
-                    (parsec-many (parsec-whitespace)))))
+  (parsec-many (parsec-return
+                    (parse-value-change)
+                  (parsec-many (parsec-whitespace)))))
 
 (defun parse-one-time-signal-dump (name-map)
   (let ((cur-time (cadr (parsec-and (parsec-many (parsec-whitespace))
                                     (parse-time)))))
-    (mapcar #'(lambda (chg) (pcase chg
-                              (`(chg ,id ,value) (let ((time-map (pcase (gethash id name-map)
-                                                                   (`(,_ ,_ ,_ ,t) t))))
-                                                   (puthash cur-time value time-map)))))
-            (parse-value-changes))))
+    (mapc #'(lambda (chg) (pcase chg
+                            (`(chg ,id ,value) (let ((time-map (destructuring-bind (_ _ _ table) (gethash id name-map)
+                                                                 table)))
+                                                 (puthash cur-time value time-map)))))
+          (parse-value-changes)))
+  nil)
 
 (defun parse-signal-dumps (name-map)
   (progn (parsec-many-till (parsec-any-ch)
                            (parsec-lookahead (parsec-str "#")))
          (parsec-many (parsec-whitespace))
          (parsec-many (parsec-return
-                             (parse-one-time-signal-dump name-map)
-                           (parsec-many (parsec-whitespace))))))
+                          (parse-one-time-signal-dump name-map)
+                        (parsec-many (parsec-whitespace))))
+         nil))
+
+
+(defun parse-enddefinitions ()
+  (progn (parsec-str "$enddefinitions")
+         (parsec-many1 (parsec-whitespace))
+         (parsec-str "$end"))
+  nil)
+
+(defun parse-dumpvars (name-map)
+  (let (
+        ;; (_ (parsec-many-till (parsec-any-ch)
+        ;;                      (parsec-lookahead (parsec-str "$dumpvars"))))
+        (_ (parsec-str "$dumpvars"))
+        (_ (parsec-many1 (parsec-whitespace)))
+        (vcs (parse-value-changes))
+        (_ (parsec-str "$end"))
+        )
+    (mapc #'(lambda (chg)
+                (pcase chg
+                  (`(chg ,id ,value)
+                   (let ((time-map (destructuring-bind (_ _ _ table) (gethash id name-map)
+                                     table)))
+                     (puthash 'init value time-map)))))
+            vcs)))
 
 (defun parse-vcd ()
   (let* ((header (parse-vcd-header))
          (_ (parsec-many (parsec-whitespace)))
          (m (parse-vcd-namemap))
          (_ (parsec-many (parsec-whitespace)))
-         (_ (parse-signal-dumps m)))
+         (_ (parsec-option nil (parse-enddefinitions)))
+         (_ (parsec-many (parsec-whitespace)))
+         (_ (parsec-option nil (parse-dumpvars m)))
+         (_ (parse-signal-dumps m))
+         )
     m))
 
-(parsec-with-input sample-vcd (parse-vcd))
+(defvar *namemap* (parsec-with-input sample-vcd (parse-vcd)))
+
+(hash-table-p *namemap*)
+
+(gethash "|s" *namemap*)
+(gethash ":g" *namemap*)
+
+(defmacro get-rest (p)
+  `(parsec-with-input ,(cadr p)
+     (substring ,(cadr p) (1- (parsec-query ,(caddr p) :end)) (1- (length ,(cadr p))))))
+
+(defvar *remainder* (get-rest (parsec-with-input sample-vcd (parse-vcd))))
+
+(parsec-with-input *remainder* (parse-value-changes))
+
+(substring  (get-rest (parse-with-input *remainder* (parse-dumpvars *namemap*))) 0 100)
+
+(defvar *new-rem* (get-rest (parsec-with-input *remainder* (parse-signal-dumps *namemap*))))
+
+(substring *new-rem* 0 14)
+
+(substring (get-rest (parsec-with-input *remainder* (progn  (parse-enddefinitions)
+                                                            (parsec-many (parsec-whitespace))
+                                                            (parsec-option nil (parse-dumpvars *namemap*))))) 0 100)
+
+(defvar *new-rem* (get-rest (parsec-with-input *remainder*
+                              (parsec-and (parsec-until (parsec-lookahead (parsec-str "#")))
+                                          (parsec-count 3
+                                                        (progn  (parse-time)
+                                                                (parsec-many1
+                                                                 (parsec-return (parse-value-change)
+                                                                   (parsec-or (parsec-try (parsec-many (parsec-whitespace)))
+                                                                              (parsec-try (parsec-eof))))))
+                                                        )
+                                          ))))
+
+(substring *new-rem* 0 100)
+
+
+(parsec-with-input "b000000000000000000000000 $" (parse-value-change))
 
 (defun vcd-parse-all-signal-traces (vcd-text)
   "TODO Dumps trace data for every traced signal in one pass.
    Costly for space when the trace is very large."
-  (let (name-map (make-hash-table))
+  (let (name-map (make-hash-table :test 'equal))
     (parsec-with-input vcd-text
       (parsec-many-till
        (parsec-any-ch)
